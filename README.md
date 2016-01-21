67d24e25a441110dec175018e628d8a6a36b18a5
root <root@library-03.cse.iitb.ac.in>
2016-01-22 00:00:24 +0530

    updating readme

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-Test readme
+
+Used RWMutex for mutual exclusion. Write lock used for delete, cas and write commands.
+Read lock used for read command.
+
+Assuming one command per connection at server side to avoid complexity as errors from 
+first command may affect next commands on same connection.
+
+
+To enable server-side logging, uncomment "log" package & replace all "// log." with "log." in server.go
--- assignment1/server.go
+++ assignment1/server.go
@@ -5,7 +5,7 @@ import (
 	"net"
 	"strings"
 	"bufio"
-	"log"
+	//"log"
 	//"bytes"
 	"strconv"
 	"math/rand"
@@ -42,7 +42,7 @@ func handleWrite(command []string, conn net.Conn, reader *bufio.Reader){
 
 	// Invalid number of arguments
 	if len(command) != 3 && len(command) != 4 {
-		log.Printf("[SERVER]:[ERR]:invalid command arguments")
+		// log.Printf("[SERVER]:[ERR]:invalid command arguments")
 		conn.Write([]byte("ERR_CMD_ERR\r\n"))
 		return
 	}
@@ -50,18 +50,18 @@ func handleWrite(command []string, conn net.Conn, reader *bufio.Reader){
 	filename := strings.TrimSpace(command[1])
 	numbytes, err := strconv.ParseUint(command[2], 10, 64)
 	if err != nil {
-		log.Printf("[SERVER]:[ERR]:unable to parse <numbytes> field: %v",command[2])
+		// log.Printf("[SERVER]:[ERR]:unable to parse <numbytes> field: %v",command[2])
 		conn.Write([]byte("ERR_CMD_ERR\r\n"))
 		return
 	}
 
 	buff := make([]byte, numbytes+2)
-	log.Printf("[SERVER]:[INFO]:Going to read %v bytes",numbytes+2)
+	// log.Printf("[SERVER]:[INFO]:Going to read %v bytes",numbytes+2)
 	n, err := io.ReadFull(reader, buff)
-	log.Printf("[SERVER]:[INFO]:Content received:%v",string(buff[:n]))
+	// log.Printf("[SERVER]:[INFO]:Content received:%v",string(buff[:n]))
 	// If unable to read numbytes bytes or if "\r\n" is not at the end of the buff
 	if err != nil || (buff[n-2]!='\r' || buff[n-1]!='\n'){
-		log.Printf("[SERVER]:[ERR]:insufficient data received or data doesn't end with \\r\\n")
+		// log.Printf("[SERVER]:[ERR]:insufficient data received or data doesn't end with \\r\\n")
 		conn.Write([]byte("ERR_CMD_ERR\r\n"))
 		return
 	}
@@ -73,13 +73,13 @@ func handleWrite(command []string, conn net.Conn, reader *bufio.Reader){
 	if len(command) == 4 {
 		exptime, err = strconv.ParseUint(command[3],0,64)
 		if err != nil {
-			log.Printf("[SERVER]:[ERR]:unable to parse <exptime> field: %v",command[3])
+			// log.Printf("[SERVER]:[ERR]:unable to parse <exptime> field: %v",command[3])
 			conn.Write([]byte("ERR_CMD_ERR\r\n"))
 			return
 		}
 	}
 
-	log.Printf("[SERVER]:[INFO]:Writing to file %v",filename)
+	// log.Printf("[SERVER]:[INFO]:Writing to file %v",filename)
 
 	// TODO:: Trim last "\r\n" from buff
 
@@ -110,9 +110,9 @@ func handleRead(cmd []string, conn net.Conn){
 	if  ok {
 		reply := fmt.Sprintf("CONTENTS %v %v %v\r\n%s\r\n", file.version, file.numbytes, file.exptime, file.contents)
 		fmt.Fprintf(conn, reply)
-		log.Printf("[SERVER]:[INFO]:Reply:%v", reply)
+		// log.Printf("[SERVER]:[INFO]:Reply:%v", reply)
 	} else {
-		log.Printf("[SERVER]:[ERR]:file not found: %v",filename)
+		// log.Printf("[SERVER]:[ERR]:file not found: %v",filename)
 		conn.Write([]byte("ERR_FILE_NOT_FOUND\r\n"))
 	}
 	return
@@ -137,7 +137,7 @@ func handleDelete(cmd []string, conn net.Conn){
 	if ok {
 		delete(m, filename)
 		lock.Unlock()
-		log.Printf("[SERVER]:[INFO]:File deleted %v",filename)
+		// log.Printf("[SERVER]:[INFO]:File deleted %v",filename)
 		fmt.Fprintf(conn, "OK\r\n")   			
 	} else {
 		lock.Unlock()
@@ -162,7 +162,7 @@ func handleCas(cmd []string, conn net.Conn,  reader *bufio.Reader){
 	//numbytes, err := strconv.Atoi(cmd[3])	
 	numbytes, err := strconv.ParseUint(cmd[3], 10, 64)
 	if err != nil {
-		log.Printf("[SERVER]:[ERR]:Unable to parse <numbytes> field")
+		// log.Printf("[SERVER]:[ERR]:Unable to parse <numbytes> field")
 		conn.Write([]byte("ERR_CMD_ERR\r\n"))
 		return
 	}
@@ -171,7 +171,7 @@ func handleCas(cmd []string, conn net.Conn,  reader *bufio.Reader){
 	version, err := strconv.ParseInt(cmd[2], 10, 64)
 	//ver, err := strconv.Atoi(cmd[2]) 
 	if err != nil {
-		log.Printf("[SERVER]:[ERR]:Unable to parse <version> field")
+		// log.Printf("[SERVER]:[ERR]:Unable to parse <version> field")
 		conn.Write([]byte("ERR_CMD_ERR\r\n"))
 		return
 	}
@@ -181,19 +181,19 @@ func handleCas(cmd []string, conn net.Conn,  reader *bufio.Reader){
 		// Parse <exptime>
 		exptime,err = strconv.ParseInt(cmd[4],0,64)
 		if err != nil {
-			log.Printf("[SERVER]:[ERR]:Unable to parse <exptime> field")
+			// log.Printf("[SERVER]:[ERR]:Unable to parse <exptime> field")
 			conn.Write([]byte("ERR_CMD_ERR\r\n"))
 			return
 		}
 	}
 
 	buff := make([]byte, numbytes+2)
-	log.Printf("[SERVER]:[INFO]:Going to read %v bytes",numbytes+2)
+	// log.Printf("[SERVER]:[INFO]:Going to read %v bytes",numbytes+2)
 	n, err := io.ReadFull(reader, buff)
-	log.Printf("[SERVER]:[INFO]:Content received:%v",string(buff[:n]))
+	// log.Printf("[SERVER]:[INFO]:Content received:%v",string(buff[:n]))
 	// If unable to read numbytes bytes or if "\r\n" is not at the end of the buff
 	if err != nil || (buff[n-2]!='\r' || buff[n-1]!='\n'){
-		log.Printf("[SERVER]:[ERR]:insufficient data received or data doesn't end with \\r\\n")
+		// log.Printf("[SERVER]:[ERR]:insufficient data received or data doesn't end with \\r\\n")
 		conn.Write([]byte("ERR_CMD_ERR\r\n"))
 		return
 	}
@@ -208,32 +208,32 @@ func handleCas(cmd []string, conn net.Conn,  reader *bufio.Reader){
 			fmt.Fprintf(conn, "OK %v\r\n", newVersion)					
 		} else {
 			lock.Unlock()
-			log.Printf("[SERVER]:[ERR]:version missmatch field")
+			// log.Printf("[SERVER]:[ERR]:version missmatch field")
 			fmt.Fprintf(conn, "ERR_VERSION\r\n")
 		}
 	} else {
 		lock.Unlock()
-		log.Printf("[SERVER]:[ERR]:file not found")
+		// log.Printf("[SERVER]:[ERR]:file not found")
 		conn.Write([]byte("ERR_FILE_NOT_FOUND\r\n"))
 	}
 	return
 }
 
 func serverMain() {
-	log.Printf("[SERVER]:[INFO]:Launching server...")
+	// log.Printf("[SERVER]:[INFO]:Launching server...")
 	listener,err := net.Listen("tcp", PORT)   
 
 	if err != nil {
-		log.Fatalf("[SERVER]:[ERR]:Error in listener: %v", err.Error())		
+		// log.Fatalf("[SERVER]:[ERR]:Error in listener: %v", err.Error())		
 	}
 
-	log.Printf("[SERVER]:[INFO]:Server started on %v", PORT)
+	// log.Printf("[SERVER]:[INFO]:Server started on %v", PORT)
 	// infinite loop until ctr-c or exit
 	for {
 		conn,err := listener.Accept()
 
 		if err != nil {
-			log.Printf("[SERVER]:[ERR]:Error in accepting a connection: %v", err.Error())
+			// log.Printf("[SERVER]:[ERR]:Error in accepting a connection: %v", err.Error())
 		} else {
 			go processClient(conn)
 		}
@@ -247,7 +247,7 @@ func processClient(conn net.Conn) {
 	for{
 		line, isPrefix, err := reader.ReadLine()	// Read until \n or \r\n
 		if err != nil || isPrefix {
-			log.Printf("[SERVER]:[ERR]:Error in reading from client: %v", err.Error())
+			// log.Printf("[SERVER]:[ERR]:Error in reading from client: %v", err.Error())
 			errMsg := "ERR_CMD_ERR\r\n"
 			conn.Write([]byte(errMsg))
 			return
@@ -255,12 +255,12 @@ func processClient(conn net.Conn) {
 
 		command := strings.Fields(string(line))
 		if len(command) == 0 {
-			log.Printf("[SERVER]:[ERR]:null command string received")
+			// log.Printf("[SERVER]:[ERR]:null command string received")
 			conn.Write([]byte("ERR_CMD_ERR\r\n"))
 			return
 		}
 
-		log.Printf("[SERVER]:[INFO]:Command : %v",strings.TrimSpace(string(line)))
+		// log.Printf("[SERVER]:[INFO]:Command : %v",strings.TrimSpace(string(line)))
 		switch command[0] {
 			case "read":
 				handleRead(command, conn)
@@ -275,10 +275,10 @@ func processClient(conn net.Conn) {
 				handleDelete(command, conn)
 
 			default:
-				log.Printf("[SERVER]:[ERR]:invalid command: %v",command[0])
+				// log.Printf("[SERVER]:[ERR]:invalid command: %v",command[0])
 				conn.Write([]byte("ERR_CMD_ERR\r\n"))
 		}
-		log.Printf("[SERVER]:[INFO]:Served : %v",strings.TrimSpace(string(line)))
+		// log.Printf("[SERVER]:[INFO]:Served : %v",strings.TrimSpace(string(line)))
 	}
 }
 
