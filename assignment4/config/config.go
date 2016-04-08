20c9f852cc3bc1f85fe3448570536f794b13139d
abhishekvg <abhishekvg@cse.iitb.ac.in>
2016-04-08 23:30:40 +0530

    Created global config package   - Contains Config structure, contains all necessary fields required to restore a server from last crash point Moved Config structure from StateMachine to this config Moved ToConfigFile and FromConfigFile functions to this package

---
+++ assignment4/config/config.go
@@ -0,0 +1,45 @@
+package config
+
+import (
+	"os"
+	"encoding/json"
+	"github.com/cs733-iitb/cluster/mock"
+)
+
+type Config struct {
+	Id               int    // this node's id. One of the cluster's entries should match.
+	LogDir           string // Log file directory for this node
+	ElectionTimeout  int
+	HeartbeatTimeout int
+	NumOfNodes       int
+	MockServer       *mock.MockServer
+}
+
+
+func ToConfigFile(configFile string, config Config) (err error) {
+	var f *os.File
+	if f, err = os.Create(configFile); err != nil {
+		return err
+	}
+	defer f.Close()
+	enc := json.NewEncoder(f)
+	if err = enc.Encode(config); err != nil {
+		return err
+	}
+	return nil
+}
+
+func FromConfigFile(configFile string) (config *Config, err error) {
+	var cfg Config
+	var f *os.File
+	if f, err = os.Open(configFile); err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	dec := json.NewDecoder(f)
+	if err = dec.Decode(&cfg); err != nil {
+		return nil, err
+	}
+	return &cfg, nil
+}
+
